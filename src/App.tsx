6a1236f704fe00d7e52f44f63364669eca19538f
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2022-11-18 18:36:59 +0100

    🚧 Scaffold solution
    Making scalleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="flex mobile-only:p-0 p-5 h-screen w-screen rounded border border-red-600">
-        Kalle
+      <div className=" h-screen w-screen p-2">
+<div className="border-red-600 flexlex-col p-2 rounded border-4 border-wcbeige-default h-full w-full">
+
+          <div className="flex">Cash</div>
+          <div className="flex">Fotballspiller</div>
+
+          <div className="">Liste over spillere</div>
+          <div className="flex">Siste resultater</div>
+          <div className="flex">Neste kamper</div>
+
+          <div className="flex">Kommune logo</div>
+          <span className="flex">sist oppdatert</span>
+          <div className="flex">Wc Logo</div>
+</div>
       </div>
     </div>
   );
