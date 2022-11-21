ecf6151b96029890fc64dfe477eba699a9ef4f86
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2022-11-21 23:08:36 +0100

    💩 update
    updating

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ import Wclogo from "./wclogo/Wclogo";
 function App() {
   return (
     <div className="App">
-      <div className=" min-h-screen min-w-max p-2">
-        <div className="border-red-600  flex h-full w-full flex-col  rounded border-4 border-wcbeige-default p-2 ">
+      <div className=" flex min-h-screen w-full min-w-full p-2">
+        <div className="border-red-600 flex h-full min-h-screen w-full min-w-full flex-col grow rounded border-4 border-wcbeige-default p-2 ">
           <div className="flex w-full justify-between justify-items-end">
             <TakeMyMoney />
             <div className="flex text-center text-5xl text-wcbeige-default">
@@ -26,7 +26,7 @@ function App() {
             <Nintendowcspiller />
           </div>
 
-          <div className="grid grow w-full grid-cols-3 justify-between text-2xl">
+          {/* <div className="grid w-full grow grid-cols-3 justify-between text-2xl">
             <div className="m-4 mt-12 ml-12 flex">
               <ul>
                 {data.scores
@@ -40,21 +40,22 @@ function App() {
                       score: Array<Number> | any;
                     }) => (
                       <li key={item.name}>
-                        {item.ranking}. {item.name} - {item.score[item.score.length - 1]}
+                        {item.ranking}. {item.name}:{" "}
+                        {item.score.reduce((a: number, b: number) => a + b, 0)}{" "}
+                        ({item.score[item.score.length - 1]})
                       </li>
                     )
                   )}
               </ul>
             </div>
             <div className="m-4 flex flex-col ">
-
               <div className="flex justify-center">
                 <ul>
                   {data.scores
                     .sort(function (a, b) {
                       return a.ranking - b.ranking;
                     })
-                    .slice(0, 4)
+                    .slice(0, 3)
                     .map(
                       (item: {
                         name: string;
@@ -64,7 +65,7 @@ function App() {
                         <li key={item.name}>
                           <div className="relative">
                             <img
-                              className="h-48 scale-90 hover:scale-100 ease-in duration-500"
+                              className="h-48 scale-90 duration-500 ease-in hover:scale-100"
                               src={`/profiles/card/${item.name
                                 .toLowerCase()
                                 .replace(/\s/g, "")}.png`}
@@ -81,22 +82,26 @@ function App() {
               <div className="m-4 ">
                 <p>Siste resultater:</p>
                 <ul>
-                  <li>Qatar 0 - 2 Ecuador</li>
+                  <li>England 6 - 2 Iran*</li>
+                  <li>Senegal 0 - 2 Nederland*</li>
+                  <li>USA 1 - 1 Wales</li>
                 </ul>
               </div>
               <div className="m-4">
                 <p>Neste kamper:</p>
                 <ul>
-                  <li>England - Iran</li>
-                  <li>Senegal - Nederland</li>
-                  <li>USA - Wales</li>
+                  <li>11:00 : Argentina - Saudi Arabia*</li>
+                  <li>14:00 : Danmark - Tunisia</li>
+                  <li>17:00 : Mexico - Polen*</li>
+                  <li>20:00 : Frankrike - Australia</li>
                 </ul>
+                <p>* Tellende kamper i konkurransen</p>
               </div>
             </div>
-          </div>
+          </div> */}
           <div className="flex h-14 w-full items-center justify-between self-end text-wcbeige-default">
             <Oklogo />
-            <span className="text-center">Sist oppdatert: 20.11.22</span>
+            <span className="text-center">Sist oppdatert: 21.11.22</span>
             <Wclogo />
           </div>
         </div>
