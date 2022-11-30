e56d35e2ad412a9a02da9f97166e92b97257fff3
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2022-12-01 00:22:35 +0100

    ✏️ Fix typo

--- src/siste-resultater/SisteResultater.tsx
+++ src/siste-resultater/SisteResultater.tsx
@@ -5,19 +5,38 @@ const SisteResultater = () => {
     <table className="table-auto">
       <thead>
         <tr>
-          <th colSpan={2} className="text-3xl text-left">
+          <th colSpan={2} className="text-left text-3xl">
             Siste resultater:
           </th>
         </tr>
       </thead>
       <tbody>
-        <SisteResultatKort hjemmelag="Tunisa" hjemmelagScore={1} bortelag="Frankrike" bortelagScore={0} />
-        <SisteResultatKort hjemmelag="Australia" hjemmelagScore={1} bortelag="Danmark" bortelagScore={0} />
-        <SisteResultatKort hjemmelag="Polen" hjemmelagScore={0} bortelag="Argentina" bortelagScore={2} />
-        <SisteResultatKort hjemmelag="Saudi-Arabia" hjemmelagScore={1} bortelag="USA" bortelagScore={2} />
+        <SisteResultatKort
+          hjemmelag="Tunisa"
+          hjemmelagScore={1}
+          bortelag="Frankrike"
+          bortelagScore={0}
+        />
+        <SisteResultatKort
+          hjemmelag="Australia"
+          hjemmelagScore={1}
+          bortelag="Danmark"
+          bortelagScore={0}
+        />
+        <SisteResultatKort
+          hjemmelag="Polen"
+          hjemmelagScore={0}
+          bortelag="Argentina"
+          bortelagScore={2}
+        />
+        <SisteResultatKort
+          hjemmelag="Saudi-Arabia"
+          hjemmelagScore={1}
+          bortelag="Mexico"
+          bortelagScore={2}
+        />
       </tbody>
     </table>
   );
-
 };
 export default SisteResultater;
