792a2fb31aed73d85b0c23db0c50d87cc8ebd575
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2022-11-30 10:17:03 +0100

    ✅ Just small update for test

--- src/neste-kamper/NestekampKort.tsx
+++ src/neste-kamper/NestekampKort.tsx
@@ -1,4 +1,3 @@
-
 const NestekampKort = (props: {
   tidspunkt: string;
   hjemmelag: string;
@@ -9,30 +8,28 @@ const NestekampKort = (props: {
   style?: string;
 }): JSX.Element => {
   return (
-
-    <div className="grid grid-cols-4 grid-rows-2 mt-2">
+    <div className="mt-2 grid grid-cols-4 grid-rows-2">
       <div className="flex w-fit text-left">{props.tidspunkt}:</div>
-      <div className="flex w-full justify-start underline items-center font-medium decoration-wcbeige-50">
-       {props.hjemmelag}
+      <div className="flex w-full items-center justify-start font-medium underline decoration-wcbeige-50">
+        {props.hjemmelag}
       </div>
-      <div className="flex w-full justify-center  ">-</div>
+      <div className="flex w-full justify-center">-</div>
       <div className="flex w-full justify-end font-medium underline decoration-wcblue-50">
         {props.bortelag}
       </div>
-      <div className="w-full mt-2 text-sm ">🔮</div>
+      <div className="mt-2 w-full text-sm ">🔮</div>
       <div className="col-span-3 mt-2 flex h-6">
         <div
-          className="text-white-default flex w-full justify-center items-center text-white rounded-l-lg bg-wcbeige-50 text-sm"
+          className="text-white flex w-full items-center justify-center rounded-l-lg bg-wcbeige-50 text-sm text-white-default"
           style={{ width: `${props.prediksjonH}%` }}
         >
           {props.prediksjonH}%
         </div>
         <div
-          className="text-white-default flex w-full items-center justify-center bg-wcyellow-300 text-sm "
+          className="flex w-full items-center justify-center bg-wcyellow-300 text-sm text-white-default "
           style={{ width: `${props.prediksjonU}%` }}
         >
-
-          {props.prediksjonU === '0' ? '' : `${props.prediksjonU}%` }
+          {props.prediksjonU === "0" ? "" : `${props.prediksjonU}%`}
         </div>
         <div
           className="flex w-full items-center justify-center rounded-r-lg bg-wcblue-100 text-sm text-white-default"
