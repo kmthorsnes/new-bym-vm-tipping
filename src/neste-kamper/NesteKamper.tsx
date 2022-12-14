857008ec92faae6dcc76f0f312735b03ed802084
Karl-Magnus Thorsnes <kmthorsnes@gmail.com>
2022-12-14 23:42:57 +0100

    🌱 UPdate

--- src/neste-kamper/NesteKamper.tsx
+++ src/neste-kamper/NesteKamper.tsx
@@ -1,43 +1,106 @@
-import { useState, useEffect } from "react";
-import NestekampKort from "./NestekampKort";
+import { useState } from "react";
+import { useEffect } from "react";
+import SisteResultatKort from "../siste-resultater/SisteResultatKort";
 
-const NesteKamper = () => {
-  const getData = async (url: string) => {
-    const response = await fetch(url);
-    return response.json();
-  };
-
-  const [apidata, setApiData] = useState<any[]>([]);
-
+const SisteResultater = () => {
+  // Fetch the latest completed matches from the API
+  const [matches, setMatches] = useState<any[]>([]);
   useEffect(() => {
-    getData("https://worldcupjson.net/matches").then((apidata) =>
-      setApiData(apidata)
-    );
+    async function fetchMatches() {
+      const response = await fetch("https://worldcupjson.net/matches");
+      const matches = await response.json();
+      // Filter out only the latest completed matches
+      const latestCompletedMatches = matches.filter(
+        (match: {
+          status: string;
+          datetime: string | number | Date | any;
+          home_team: any;
+          away_team: any;
+          stage_name: string;
+        }) => {
+          // Check that the match is completed and is from the last 5 days
+          return (
+            match.status !== "completed"
+          );
+
+        }
+      );
+      setMatches(latestCompletedMatches);
+    }
+    fetchMatches();
   }, []);
 
-  // return a list of latest played matches
-  const latestMatches = apidata
-    .filter((match) => match.status === "completed")
-    .slice(0, 5);
-
-  // return a list of upcoming matches
+  // Render the matches in the table
   return (
-    <>
-      <div className="table-auto">
-        <div className="text-left text-3xl mb-2 font-bold">Neste kamper - Semifinaler</div>
-      </div>
-      <div>
-        <div className="text-xl mt-2">Onsdag 14.12.2022</div>
-        <NestekampKort
-          tidspunkt="20:00"
-          hjemmelag="Frankrike"
-          bortelag="Marokko"
-          prediksjonH="50"
-          prediksjonU="25"
-          prediksjonB="25"
-        />
-      </div>
-    </>
+    <table className="table-auto">
+      <thead>
+        <tr>
+          <th colSpan={2} className="text-left text-2xl">
+            Neste kamper: Finale og BronseFinale
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {matches.map(
+          (
+            match // set any type for home_team and away_team
+          ) => (
+            <SisteResultatKort
+              hjemmelag={match.home_team.name}
+              hjemmelagScore={match.home_team.goals}
+              bortelag={match.away_team.name}
+              bortelagScore={match.away_team.goals}
+            />
+          )
+        )}
+      </tbody>
+    </table>
   );
 };
-export default NesteKamper;
+
+export default SisteResultater;
+
+
+// import { useState, useEffect } from "react";
+// import NestekampKort from "./NestekampKort";
+
+// const NesteKamper = () => {
+//   const getData = async (url: string) => {
+//     const response = await fetch(url);
+//     return response.json();
+//   };
+
+//   const [apidata, setApiData] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     getData("https://worldcupjson.net/matches").then((apidata) =>
+//       setApiData(apidata)
+//     );
+//   }, []);
+
+//   // return a list of latest played matches
+//   const latestMatches = apidata
+//     .filter((match) => match.status === "completed")
+//     .slice(0, 5);
+
+//   // return a list of upcoming matches
+//   return (
+//     <>
+//       <div className="table-auto">
+//         <div className="text-left text-3xl mb-2 font-bold">Neste kamper - Semifinaler</div>
+//       </div>
+//       <div>
+//         <div className="text-xl mt-2">Onsdag 14.12.2022</div>
+//         <NestekampKort
+//           tidspunkt="20:00"
+//           hjemmelag="Frankrike"
+//           bortelag="Marokko"
+//           prediksjonH="50"
+//           prediksjonU="25"
+//           prediksjonB="25"
+//         />
+//       </div>
+//     </>
+//   );
+// };
+// export default NesteKamper;
--- src/resultatliste/Resultatliste.tsx
+++ src/resultatliste/Resultatliste.tsx
@@ -48,78 +48,81 @@ const sortByTotalScore = () => {
 // a function that ranks players after totalt socre. If two players or more have the same score, they get the same ranking and the next players gets a rank which is the previous score + the number of players with the previous score.
 
 const addRanking = () => {
-  let ranking = 1;
-  let previousScore = 0;
-  let numberOfPlayersWithPreviousScore = 0;
-  data.scores.forEach((item, index) => {
-    if (
-      item.groupStageScore +
-        item.groupStageXtra +
-        item.eightsMatchesPt1 +
-        item.eightsMatchesPt2 +
-        item.quarterFinalists +
-        item.qf1 +
-        item.qf2 +
-        item.qf3 +
-        item.qf4 +
-        item.semiFinalist1 +
-        item.semiFinalist2 +
-        item.semiFinalist3 +
-        item.semiFinalist4 +
-        item.finalist1 +
-        item.finalist2 +
-        item.champion ===
-      previousScore
-    ) {
-      item.ranking = ranking;
-      numberOfPlayersWithPreviousScore++;
-    }
-    if (
-      item.groupStageScore +
-        item.groupStageXtra +
-        item.eightsMatchesPt1 +
-        item.eightsMatchesPt2 +
-        item.quarterFinalists +
-        item.qf1 +
-        item.qf2 +
-        item.qf3 +
-        item.qf4 +
-        item.semiFinalist1 +
-        item.semiFinalist2 +
-        item.semiFinalist3 +
-        item.semiFinalist4 +
-        item.sf1 +
-        item.sf2 +
-        item.finalist1 +
-        item.finalist2 +
-        item.champion !==
-      previousScore
-    ) {
-      ranking = ranking + numberOfPlayersWithPreviousScore;
-      item.ranking = ranking;
-      numberOfPlayersWithPreviousScore = 1;
-      previousScore =
-        item.groupStageScore +
-        item.groupStageXtra +
-        item.eightsMatchesPt1 +
-        item.eightsMatchesPt2 +
-        item.quarterFinalists +
-        item.qf1 +
-        item.qf2 +
-        item.qf3 +
-        item.qf4 +
-        item.semiFinalist1 +
-        item.semiFinalist2 +
-        item.semiFinalist3 +
-        item.semiFinalist4 +
-        item.sf1 +
-        item.sf2 +
-        item.finalist1 +
-        item.finalist2 +
-        item.champion
+  data.scores.sort((a, b) => {
+    const scoreA = a.groupStageScore +
+      a.groupStageXtra +
+      a.eightsMatchesPt1 +
+      a.eightsMatchesPt2 +
+      a.quarterFinalists +
+      a.qf1 +
+      a.qf2 +
+      a.qf3 +
+      a.qf4 +
+      a.semiFinalist1 +
+      a.semiFinalist2 +
+      a.semiFinalist3 +
+      a.semiFinalist4 +
+      a.sf1 +
+      a.sf2 +
+      a.finalist1 +
+      a.finalist2 +
+      a.champion;
+    const scoreB = b.groupStageScore +
+      b.groupStageXtra +
+      b.eightsMatchesPt1 +
+      b.eightsMatchesPt2 +
+      b.quarterFinalists +
+      b.qf1 +
+      b.qf2 +
+      b.qf3 +
+      b.qf4 +
+      b.semiFinalist1 +
+      b.semiFinalist2 +
+      b.semiFinalist3 +
+      b.semiFinalist4 +
+      b.sf1 +
+      b.sf2 +
+      b.finalist1 +
+      b.finalist2 +
+      b.champion;
+    if (scoreA < scoreB) {
+      return 1;
+    } else if (scoreA > scoreB) {
+      return -1;
+    } else {
+      return 0;
     }
   });
+
+  let previousScore = -1;
+  let ranking = 1;
+  data.scores.forEach((item, index) => {
+    const score = item.groupStageScore +
+      item.groupStageXtra +
+      item.eightsMatchesPt1 +
+      item.eightsMatchesPt2 +
+      item.quarterFinalists +
+      item.qf1 +
+      item.qf2 +
+      item.qf3 +
+      item.qf4 +
+      item.semiFinalist1 +
+      item.semiFinalist2 +
+      item.semiFinalist3 +
+      item.semiFinalist4 +
+      item.sf1 +
+      item.sf2 +
+      item.finalist1 +
+      item.finalist2 +
+      item.champion;
+    if (score !== previousScore) {
+      ranking = index + 1;
+      previousScore = score;
+    }
+    item.ranking = ranking;
+  });
 };
+
 sortByTotalScore();
 addRanking();
 
@@ -187,7 +190,7 @@ const Resultatliste = () => {
                   item.finalist2 + item.champion
                    }
                 {"  "}
-                <span className="text-xs">({item.sf1})</span>
+                <span className="text-xs">({item.sf2})</span>
                 <span className="text-sm">
                   [
                   <span className="text-bumanguéSBlue-200">
